31faa270c5b5e323fa9f238b04a97b8b723fff68
atif <ratul0947@gmail.com>
2022-01-24 20:42:58 +0600

    flatlist horizontal list

--- src/screens/FlatDemo.js
+++ src/screens/FlatDemo.js
@@ -3,30 +3,28 @@ import React from "react";
 
 const FlatDemo = () => {
 	const names = [
-		{
-			name: "Atif",
-		},
-		{
-			name: "Aslam",
-		},
-		{
-			name: "ratul",
-		},
-		{
-			name: "sharna",
-		},
+		{ id: 1, name: "Atif" },
+		{ id: 2, name: "Aslam" },
+		{ id: 3, name: "ratul" },
+		{ id: 4, name: "sharna" },
+		{ id: 5, name: "sharna" },
+		{ id: 6, name: "sharna" },
+		{ id: 7, name: "sharna" },
+		{ id: 8, name: "sharna" },
 	];
 
 	return (
 		<FlatList
+			style={styles.listStyles}
+			keyExtractor={(item) => item.id}
 			data={names}
+			horizontal
+			inverted
+			// numColumns={2}
+			showsHorizontalScrollIndicator={false}
 			renderItem={(element) => {
 				console.log(element);
-				return (
-					<Text style={styles.textStyle} key={element.index}>
-						{element.item.name}
-					</Text>
-				);
+				return <Text style={styles.textStyle}>{element.item.name}</Text>;
 			}}
 		/>
 	);
@@ -34,7 +32,16 @@ const FlatDemo = () => {
 
 const styles = StyleSheet.create({
 	textStyle: {
-		fontSize: 30,
+		fontSize: 20,
+		backgroundColor: "red",
+		padding: 30,
+		margin: 10,
+		color: "white",
+	},
+	listStyles: {
+		textAlign: "center",
+		margin: 20,
+		padding: 10,
 	},
 });
 
